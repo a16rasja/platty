bf417fbc6ae505ba4d6ddd3f7db52c749532cec8
Rasmus Jarenmark <rjarenmark@gmail.com>
2020-08-06 23:55:09 +0200

    Added timer when key is pressed for returning to hub

---
+++ Assets/Timer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Slider slider;
+
+    // Function for setting value on timer
+    public void SetTime(int time)
+    {
+        slider.value = time;
+    }
+}
